a2ee13194f288e6c16285315ea17932faeca73d1
enbohm <andreas@enbohms.se>
2013-04-10 09:50:59 +0200

    Renamed file

---
+++ src/se/enbohms/legacy/TweetService_Original.java
@@ -0,0 +1,35 @@
+package se.enbohms.legacy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import se.enbohms.legacy.exception.UserNotLoggedInException;
+import se.enbohms.legacy.tweet.Tweet;
+import se.enbohms.legacy.tweet.TweetDao;
+import se.enbohms.legacy.user.SessionService;
+import se.enbohms.legacy.user.User;
+
+public class TweetService_Original {
+
+	public List<Tweet> getTripsByUser(User user)
+			throws UserNotLoggedInException {
+		List<Tweet> tweetList = new ArrayList<Tweet>();
+		User loggedUser = SessionService.getInstance().getLoggedUser();
+		boolean isFriend = false;
+		if (loggedUser != null) {
+			for (User friend : user.getFriends()) {
+				if (friend.equals(loggedUser)) {
+					isFriend = true;
+					break;
+				}
+			}
+			if (isFriend) {
+				tweetList = TweetDao.findTweetsByUser(user);
+			}
+			return tweetList;
+		} else {
+			throw new UserNotLoggedInException();
+		}
+	}
+
+}
