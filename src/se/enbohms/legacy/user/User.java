75574960adb029cef153b39e3f3486599da7a4b3
enbohm <andreas@enbohms.se>
2013-04-16 07:39:19 +0200

    Added getFriends and getTweets methods in User class

--- src/se/enbohms/legacy/user/User.java
+++ src/se/enbohms/legacy/user/User.java
@@ -1,10 +1,25 @@
 package se.enbohms.legacy.user;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import se.enbohms.legacy.tweet.Tweet;
 
 public class User {
 
+	private List<Tweet> tweets = new ArrayList<>();
+
+	private List<User> friends = new ArrayList<User>();
+
 	public List<User> getFriends() {
-		return null;
+		return friends;
+	}
+
+	public void addFriend(User user) {
+		friends.add(user);
+	}
+
+	public void addTweet(Tweet tweet) {
+		tweets.add(tweet);
 	}
 }
